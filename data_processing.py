830687c9a401dc4805d6d4d4585f48b999d709d8
Stacy <stacyd28@gmail.com>
2024-11-12 16:12:43 +0100

    data process class

---
+++ data_processing.py
@@ -0,0 +1,33 @@
+from torch.utils.data import Dataset
+import torch
+
+class CharDataset(Dataset):
+    """
+    Emits batches of characters.
+
+    Adapted from "https://github.com/karpathy/minGPT".
+    """
+
+    def __init__(self, config, data):
+
+        with open(f'{data}', "r") as file:
+            self.data = file.read()
+        
+        # get characters from the input data
+        chars = sorted(set(self.data))
+        self.stoi = { ch:i for i,ch in enumerate(chars) } # map characters to integer indices
+
+    def get_vocab_size(self):
+        return len(self.stoi)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        # grab a chunk of (block_size + 1) characters from the data
+        # encode every character to an integer
+        # return the chunk and the shifted version as tensors
+        
+        block_size = 5
+        chunk = [self.stoi[char] for char in self.data[idx : idx+block_size+1]]
+        return torch.tensor(chunk[:-1], dtype=torch.int8), torch.tensor(chunk[1:], dtype=torch.int8)
