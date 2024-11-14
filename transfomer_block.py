a3c682d85d2f778d061404a0138bae42b030acb4
Stacy <stacyd28@gmail.com>
2024-11-14 14:00:11 +0100

    transfomer block

---
+++ transfomer_block.py
@@ -0,0 +1,26 @@
+from torch import nn
+from mha import MHA_
+from MLP import MLP_
+
+class TBlock(nn.Module):
+     
+
+     def __init__(self):
+
+        super().__init__()
+
+        self.ln1 = nn.LayerNorm(normalized_shape=64)
+        self.mha = MHA_(8)
+        self.ln2 = nn.LayerNorm(normalized_shape=64)
+        self.mlp = MLP_(50)
+
+     def forward(self, x):
+         
+        x_n = self.ln1(x)
+        x1 = self.mha(x_n)
+        x1_n = self.ln2(x1)
+        x2 = self.mlp(x1_n)
+
+        y = x2+x
+
+        return y
