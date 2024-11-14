5e0589e9954f7dc2742510d0adb5d87cb26f8be7
Stacy <stacyd28@gmail.com>
2024-11-14 14:25:01 +0100

    transfomer

---
+++ transformer.py
@@ -0,0 +1,30 @@
+from torch import nn
+from transfomer_block import TBlock
+from Embedding import Embedding_
+
+class Transformer(nn.Module):
+     
+
+    def __init__(self, dico, hidden_layer):
+
+        super().__init__()
+
+        self.emb = Embedding_(dico)
+        self.tblock = nn.Sequential(
+            *(TBlock()
+              for _ in range(4))
+        ) 
+        self.l1 = nn.Linear(64, len(dico))
+
+
+
+    def forward(self, x):
+         
+        x_emb = self.emb(x)
+        x_t = self.tblock(x_emb)
+        H = self.l1(x_t)
+
+        # attention les dimension vont peter à la gueule surement
+        y = x_t @ H
+
+        return y
